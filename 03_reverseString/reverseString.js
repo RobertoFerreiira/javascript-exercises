1c17760aec78a2273a910d94d9558a256aab09c8
RobertoFerreiira <100981281+RobertoFerreiira@users.noreply.github.com>
2023-11-02 13:39:17 -0300

    ExercisesJS

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const arrStrReverse = str.split("").reverse().join("");
+    return arrStrReverse;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
