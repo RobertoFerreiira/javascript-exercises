55164239d3e01913fefb2605f29a1592dbed920b
RobertoFerreiira <100981281+RobertoFerreiira@users.noreply.github.com>
2023-11-06 18:25:13 -0300

    exercise 6-7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  let tempCel = (tempInFahrenheit - 32) / 1.8;
+  
+  if(tempCel > 0){
+      return parseFloat(tempCel.toFixed(1));
+    } else {
+      return parseFloat(tempCel.toFixed(1));
+    }
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(tempInCelsius) {
+    let tempFah = (tempInCelsius * 1.8) + 32;
 
+    if(tempFah > 0){
+      return parseFloat(tempFah.toFixed(1));
+    } else {
+      return parseFloat(tempFah.toFixed(1));
+    }
+};
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
