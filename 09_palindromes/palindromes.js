cb8c7287f67db3e8a5d1b8f730c9395c2a945415
RobertoFerreiira <100981281+RobertoFerreiira@users.noreply.github.com>
2023-11-08 12:04:56 -0300

    exercicio 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (string) {
+    let reverseStr = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return reverseStr.split('').reverse().join('') == reverseStr;
+}
+    
 // Do not edit below this line
 module.exports = palindromes;
