6ff3bb4812d6cde6ec255c6915f344a54c59794c
RobertoFerreiira <100981281+RobertoFerreiira@users.noreply.github.com>
2023-11-09 13:11:38 -0300

    fibo

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(fibo, numIndex) {
+     num = parseInt(numIndex)
 
-};
+     for(let index = 4; index < numIndex; index++) {
+         for(let second = 5; second < numIndex; second++) {
+             fibo.push(fibo[index] + fibo[second]);
+             index++
+         }
+     }
+     if(num === 0){
+           return 0;
+     } else if(num > 0){
+           return fibo[num-1]
+     } else {
+           return "OOPS"
+     }
+   
+ };
+ console.log(fibonacci([1, 1, 2, 3, 5, 8], "1"))
 
-// Do not edit below this line
+ //Do not edit below this line
 module.exports = fibonacci;
