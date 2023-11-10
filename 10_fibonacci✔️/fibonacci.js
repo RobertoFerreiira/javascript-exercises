530e7c7ae5dd2479f6c8a763e2fae2428828f0f1
RobertoFerreiira <100981281+RobertoFerreiira@users.noreply.github.com>
2023-11-10 10:09:40 -0300

    fibo exercise complete

--- 10_fibonacci✔️/fibonacci.js
+++ 10_fibonacci✔️/fibonacci.js
@@ -1,22 +1,20 @@
 const fibonacci = function(fibo, numIndex) {
+     arr = fibo
      num = parseInt(numIndex)
-
      for(let index = 4; index < numIndex; index++) {
          for(let second = 5; second < numIndex; second++) {
-             fibo.push(fibo[index] + fibo[second]);
-             index++
-         }
+            arr.push(arr[index] + arr[second])
+            index++;
+         } 
      }
      if(num === 0){
-           return 0;
-     } else if(num > 0){
-           return fibo[num-1]
-     } else {
-           return "OOPS"
-     }
+      return 0; }
+     if(num < 0){
+      return "OOPS"}
+      return arr[num-1]
    
  };
- console.log(fibonacci([1, 1, 2, 3, 5, 8], "1"))
+ console.log(fibonacci([1, 1, 2, 3, 5, 8], 4))
 
  //Do not edit below this line
 module.exports = fibonacci;
